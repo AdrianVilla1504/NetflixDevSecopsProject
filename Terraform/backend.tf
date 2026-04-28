b500f480f480fd1ed608ad309c29652195783b1f
Adrian Villa <adriancvilla@gmail.com>
2026-04-28 07:19:49 -0500

    changes: format files

--- Terraform/backend.tf
+++ Terraform/backend.tf
@@ -1,15 +1,15 @@
 terraform {
-    required_version = "~> 1.13.3"
-    required_providers {
-        aws = {
-            source = "hashicorp/aws"
-            version = "~> 6.14.1"
-        }
+  required_version = "~> 1.13.3"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.14.1"
     }
+  }
 
-    cloud {}
+  cloud {}
 }
 
 provider "aws" {
-    region = var.aws-region
+  region = var.aws-region
 }
--- Terraform/iam.tf
+++ Terraform/iam.tf
@@ -1,6 +1,6 @@
 resource "aws_iam_role" "role" {
   name = "${local.org}-${local.project}-${local.env}-ssm-iam-role"
-  
+
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
@@ -14,7 +14,7 @@ resource "aws_iam_role" "role" {
       }
     ]
   })
-  
+
   tags = {
     Name = "${local.org}-${local.project}-${local.env}-ssm-iam-role"
     Env  = "${local.env}"
@@ -23,11 +23,11 @@ resource "aws_iam_role" "role" {
 }
 
 resource "aws_iam_role_policy_attachment" "ssm_managed_policy" {
-    role = aws_iam_role.role.name
-    policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+  role       = aws_iam_role.role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
 
-resource "aws_iam_instance_profile" "iam-instance-profile"{
-    name = "${local.org}-${local.project}-${local.env}-instance-profile"
-    role = aws_iam_role.role.name
+resource "aws_iam_instance_profile" "iam-instance-profile" {
+  name = "${local.org}-${local.project}-${local.env}-instance-profile"
+  role = aws_iam_role.role.name
 }
